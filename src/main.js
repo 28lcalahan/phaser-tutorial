28d69ee7e2d40f29169ef5fdfa7c06ac4dc47f47
lkc333 <liam@pcal.net>
2024-09-02 20:26:42 -0700

    add note about localhost

--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@
 // Go to the tutorial here:
 // https://phaser.io/tutorials/making-your-first-phaser-3-game/part1
 // and start pasting code into this file
+
+// in the bottom right corner of VSCode, look for 'go live' - click that 
+// and your browser should open to https://127.0.0.1:5500 where you'll see the game
